8263671927c355916d02f2776227a04b599b61e5
Alexanderdejongh <69797963+Alexanderdejongh@users.noreply.github.com>
2020-08-30 20:41:29 +0000

    Add files via upload

---
+++ cluster_3_opdracht-1-JS/Index.js
@@ -0,0 +1,16 @@
+let myName = 'Alexander de Jongh';
+console.log(myName);
+let count='4+4';
+var vier=4;
+var acht= vier + 4;
+console.log(count)
+console.log(vier + vier)
+console.log(acht)
+var Stringvier='4';
+var Stringacht= vier +'4';
+console.log(Stringacht - Stringvier)
+console.log(acht * Stringacht)
+
+
+var leeftijd =zevenentwintig;
+conslole.console.log(leeftijd);
