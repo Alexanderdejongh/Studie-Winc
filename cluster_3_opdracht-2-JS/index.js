d7cac874119edb62f221fa6ea484b38c3b718ffe
Alexanderdejongh <69797963+Alexanderdejongh@users.noreply.github.com>
2020-08-31 13:36:39 +0000

    Add files via upload

---
+++ cluster_3_opdracht-2-JS/index.js
@@ -0,0 +1,33 @@
+/* var youLikeMeat =true;
+
+If (youLikeMeat){
+  document.write('Here is the meatty menu...');
+} Else{
+  document.write()
+}
+
+*/
+
+const age = 18;
+const isFemale = false;
+const driverStatus = 'no-bob';
+
+if (age >= 18){
+  console.log('You can Enter');
+}
+else if (age < 18){
+  console.log('you can nog Enter!');
+}
+
+if (isFemale){
+  console.log('You can buy your tiket!');
+}
+else if (!isFemale){
+  console.log('you can not buy your tiket!');
+}
+if (driverStatus =='bob'){
+  console.log('drive save!');
+}
+else if(driverStatus !='bob'){
+  console.log('enjoy your drinks!')
+}
