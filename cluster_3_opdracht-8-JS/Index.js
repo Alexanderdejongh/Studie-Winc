d74cbdb6993444b0e0434a59d2eeecf51086abc2
Alexanderdejongh <69797963+Alexanderdejongh@users.noreply.github.com>
2020-09-03 11:54:40 +0000

    Add files via upload

---
+++ cluster_3_opdracht-8-JS/Index.js
@@ -0,0 +1,12 @@
+const colors = ["blue", "red", "orange", "yellow", "black"];
+// var i = 0;
+
+// while ((i++, i < colors.length)) {
+//     console.log(colors[i]);
+// }
+
+// // for (var i = 0; i < colors.length; i++) {
+// console.log(colors[i]);
+// }
+
+colors.forEach((element) => console.log(element));
